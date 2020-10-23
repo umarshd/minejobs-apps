89400711b2c5fdf4661dbe9eaf62bb82a725ee3f
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-23 14:08:16 +0700

    Create undanganinterview.js

---
+++ pages/undanganinterview.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import React from "react";
+import UndanganInterviewPage from "../components/dashboard/UndanganInterviewPage";
+import Sidebar from "../components/layouts/dashboard/Sidebar";
+import Header from "../components/layouts/Header";
+
+export default function undanganinterview() {
+  return (
+    <>
+      <Head>
+        <title>Undangan Interview</title>
+      </Head>
+      <Header />
+      <main>
+        <div className="container">
+          <div className="row">
+            <Sidebar active={"active"} />
+            <UndanganInterviewPage />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
