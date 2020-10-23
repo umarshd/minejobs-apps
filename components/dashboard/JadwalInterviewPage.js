215c254fc534fde852c503f1a4a0ae69672711b0
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-23 13:11:12 +0700

    Create JadwalInterviewPage.js

---
+++ components/dashboard/JadwalInterviewPage.js
@@ -0,0 +1,161 @@
+import React from 'react'
+
+export default function JadwalInterviewPage() {
+  return (
+    <>
+      <div className="col-md-7 col-lg-9">
+        <div className="alert alert-custom" role="alert">
+          Hai Budi, Silahkan cek email kamu untuk melakukan verifikasi akun
+          <a href="#">KIRIM ULANG EMAIL</a>
+        </div>
+        <div className="card-right-jadwal-interview">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <p>Jadwal Interview</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-5 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer <span>(Orangtua Group)</span>
+                        </div>
+                        <div className="tanggal">Tanggal : 12/03/2019</div>
+                        <div className="waktu">Waktu : 10.00 WIB</div>
+                        <div className="Metode">
+                          Metode Wawancara : Video Conference
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-3 mb-auto mt-auto ml-5 btn-action">
+                      <a href="#" className="btn btn-success btn-block">
+                        <img src="/assets/icon/hangouts-meet.svg" height="16" />
+                        Link Video Conference
+                      </a>
+                      <a href="#" className="btn btn-primary mt-2 btn-block">
+                        <img
+                          src="/assets/icon/google-calendar.svg"
+                          height="16"
+                        />
+                        Setel Pengingat
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-5 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer <span>(Orangtua Group)</span>
+                        </div>
+                        <div className="tanggal">Tanggal : 12/03/2019</div>
+                        <div className="waktu">Waktu : 10.00 WIB</div>
+                        <div className="Metode">
+                          Metode Wawancara : Video Conference
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-3 mb-auto mt-auto ml-5 btn-action">
+                      <a href="#" className="btn btn-success btn-block">
+                        <img src="/assets/icon/hangouts-meet.svg" height="16" />
+                        Link Video Conference
+                      </a>
+                      <a href="#" className="btn btn-primary mt-2 btn-block">
+                        <img
+                          src="/assets/icon/google-calendar.svg"
+                          height="16"
+                        />
+                        Setel Pengingat
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-5 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer <span>(Orangtua Group)</span>
+                        </div>
+                        <div className="tanggal">Tanggal : 12/03/2019</div>
+                        <div className="waktu">Waktu : 10.00 WIB</div>
+                        <div className="Metode">
+                          Metode Wawancara : Video Conference
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-3 mb-auto mt-auto ml-5 btn-action">
+                      <a href="#" className="btn btn-success btn-block">
+                        <img src="/assets/icon/hangouts-meet.svg" height="16" />
+                        Link Video Conference
+                      </a>
+                      <a href="#" className="btn btn-primary mt-2 btn-block">
+                        <img
+                          src="/assets/icon/google-calendar.svg"
+                          height="16"
+                        />
+                        Setel Pengingat
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-5 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer <span>(Orangtua Group)</span>
+                        </div>
+                        <div className="tanggal">Tanggal : 12/03/2019</div>
+                        <div className="waktu">Waktu : 10.00 WIB</div>
+                        <div className="Metode">
+                          Metode Wawancara : Video Conference
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-3 mb-auto mt-auto ml-5 btn-action">
+                      <a href="#" className="btn btn-success btn-block">
+                        <img src="/assets/icon/hangouts-meet.svg" height="16" />
+                        Link Video Conference
+                      </a>
+                      <a href="#" className="btn btn-primary mt-2 btn-block">
+                        <img
+                          src="/assets/icon/google-calendar.svg"
+                          height="16"
+                        />
+                        Setel Pengingat
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
