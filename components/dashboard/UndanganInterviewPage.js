f0fff5ecf8a40ed491af78d281dc74e23943f8c8
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-23 13:12:24 +0700

    Create UndanganInterviewPage.js

---
+++ components/dashboard/UndanganInterviewPage.js
@@ -0,0 +1,129 @@
+import React from "react";
+
+export default function UndanganInterviewPage() {
+  return (
+    <>
+      <div className="col-md-7 col-lg-9">
+        <div className="alert alert-custom" role="alert">
+          Hai Budi, Silahkan cek email kamu untuk melakukan verifikasi akun
+          <a href="#">KIRIM ULANG EMAIL</a>
+        </div>
+        <div className="card-right-jadwal-interview">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <p>Undangan Interview</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-5 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer <span>(Orangtua Group)</span>
+                        </div>
+                        <div className="tanggal">Tanggal : 12/03/2019</div>
+                        <div className="waktu">Waktu : 10.00 WIB</div>
+                        <div className="Metode">
+                          Metode Wawancara : Video Conference
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-3 mb-auto mt-auto ml-5 btn-action">
+                      <a href="#" className="btn btn-primary ">
+                        Lihat Undangan
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-5 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer <span>(Orangtua Group)</span>
+                        </div>
+                        <div className="tanggal">Tanggal : 12/03/2019</div>
+                        <div className="waktu">Waktu : 10.00 WIB</div>
+                        <div className="Metode">
+                          Metode Wawancara : Video Conference
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-3 mb-auto mt-auto ml-5 btn-action">
+                      <a href="#" className="btn btn-primary ">
+                        Lihat Undangan
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-5 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer <span>(Orangtua Group)</span>
+                        </div>
+                        <div className="tanggal">Tanggal : 12/03/2019</div>
+                        <div className="waktu">Waktu : 10.00 WIB</div>
+                        <div className="Metode">
+                          Metode Wawancara : Video Conference
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-3 mb-auto mt-auto ml-5 btn-action">
+                      <a href="#" className="btn btn-primary ">
+                        Lihat Undangan
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-5 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer <span>(Orangtua Group)</span>
+                        </div>
+                        <div className="tanggal">Tanggal : 12/03/2019</div>
+                        <div className="waktu">Waktu : 10.00 WIB</div>
+                        <div className="Metode">
+                          Metode Wawancara : Video Conference
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-3 mb-auto mt-auto ml-5 btn-action">
+                      <a href="#" className="btn btn-primary ">
+                        Lihat Undangan
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
