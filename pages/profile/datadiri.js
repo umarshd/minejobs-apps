0c79f28ae797c217d4c1b3701141604d7b780bba
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-23 14:17:20 +0700

    Create datadiri.js

---
+++ pages/profile/datadiri.js
@@ -0,0 +1,24 @@
+import React from "react";
+import DataDiriPage from "../../components/dashboard/DataDiriPage";
+import Header from "../../components/layouts/Header";
+import Sidebar from "../../components/layouts/dashboard/profile/SidebarProfile";
+import Head from "next/head";
+
+export default function datadiri() {
+  return (
+    <>
+      <Head>
+        <title>Data Diri</title>
+      </Head>
+      <Header />
+      <main>
+        <div className="container">
+          <div className="row">
+            <Sidebar active={"active"} />
+            <DataDiriPage />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
