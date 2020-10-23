e73133dd6fc4483aaa249b2e6cc5904d430b7a28
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-23 14:29:05 +0700

    Update BahasaPage.js

--- components/dashboard/profile/BahasaPage.js
+++ components/dashboard/profile/BahasaPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 export default function BahasaPage() {
   return (
@@ -82,5 +82,5 @@ export default function BahasaPage() {
         </div>
       </div>
     </>
-  )
+  );
 }
