9966f3adcdc6cfe4c61b6ef7163b8201a9ee3cc1
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-23 13:11:32 +0700

    Create LamaranTerkirimPage.js

---
+++ components/dashboard/LamaranTerkirimPage.js
@@ -0,0 +1,150 @@
+import React from 'react'
+
+export default function LamaranTerkirimPage() {
+  return (
+    <>
+      <div className="col-md-7 col-lg-9">
+        <div className="alert alert-custom" role="alert">
+          Hai Budi, Silahkan cek email kamu untuk melakukan verifikasi akun
+          <a href="#">KIRIM ULANG EMAIL</a>
+        </div>
+        <div className="card-right-cari-lowongan">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <p>Lamaran Terkirim</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-8 mb-auto mt-auto">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer
+                        </div>
+                        <div className="corporate">Orangtua Group</div>
+                        <div className="job-location">
+                          Jawa Tengah, Semarang
+                        </div>
+                        <div className="salary">
+                          Tp.4.000.000 - RP.8.000.000 / Bulan
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-8">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer
+                        </div>
+                        <div className="corporate">Orangtua Group</div>
+                        <div className="job-location">
+                          Jawa Tengah, Semarang
+                        </div>
+                        <div className="salary">
+                          Tp.4.000.000 - RP.8.000.000 / Bulan
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-8">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer
+                        </div>
+                        <div className="corporate">Orangtua Group</div>
+                        <div className="job-location">
+                          Jawa Tengah, Semarang
+                        </div>
+                        <div className="salary">
+                          Tp.4.000.000 - RP.8.000.000 / Bulan
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <img className="img-fluid" src="/bg-home.jpg" />
+                    </div>
+                    <div className="col-md-8">
+                      <div className="job-description">
+                        <div className="job-title">
+                          Full Stack Web Developer
+                        </div>
+                        <div className="corporate">Orangtua Group</div>
+                        <div className="job-location">
+                          Jawa Tengah, Semarang
+                        </div>
+                        <div className="salary">
+                          Tp.4.000.000 - RP.8.000.000 / Bulan
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row justify-content-center mt-4">
+              <div className="col-4">
+                <nav aria-label="Page navigation example ">
+                  <ul className="pagination">
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        Previous
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        1
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        2
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        3
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        Next
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
