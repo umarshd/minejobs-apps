d048a17feb94a90ce57d5f10d95b67f17a420360
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-24 17:04:51 +0700

    Update Sidebar.js

--- components/layouts/dashboard/Sidebar.js
+++ components/layouts/dashboard/Sidebar.js
@@ -1,36 +1,37 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Sidebar() {
   return (
     <>
-      <div className="col-md-5 col-lg-3">
-        <div className="card-left-bahasa">
+      <div className="col-md-5 col-lg-3 sidebar">
+        <div className="card-left-bahasa bg-light">
           <div className="container">
             <div className="link-card-left-bahasa">
-              <a href="/carilowongan">
+              <Link href="/carilowongan">
                 <i className="fas fa-search" />
                 Cari Lowongan
-              </a>
-              <a href="/editprofile">
+              </Link>
+              <Link href="/editprofile">
                 <i className="fas fa-user" />
                 Edit Profile
-              </a>
-              <a href="/lamaranterkirim">
+              </Link>
+              <Link href="/lamaranterkirim">
                 <i className="fas fa-paper-plane" />
                 Lamaran Terkirim
-              </a>
-              <a href="/undanganinterview">
+              </Link>
+              <Link href="/undanganinterview">
                 <i className="fas fa-info-circle" />
                 Undangan Interview
-              </a>
-              <a href="/jadwalinterview">
+              </Link>
+              <Link href="/jadwalinterview">
                 <i className="fas fa-calendar-day" />
                 Jadwal Interview
-              </a>
-              <a href="/lamarantersimpan">
+              </Link>
+              <Link href="/lamarantersimpan">
                 <i className="fas fa-archive" />
                 Lamaran Tersimpan
-              </a>
+              </Link>
             </div>
           </div>
         </div>
