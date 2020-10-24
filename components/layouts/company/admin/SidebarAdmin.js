ac47b270e2201f3ac1fbd22d64a9c18b2f33dc9b
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-24 19:02:52 +0700

    Create SidebarAdmin.js

---
+++ components/layouts/company/admin/SidebarAdmin.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function SidebarAdmin() {
+  return (
+    <>
+      <div className="col-md-5 col-lg-3">
+        <div className="card-left-bahasa">
+          <div className="container">
+            <div className="link-card-left-bahasa">
+              <a href="/carilowongan">
+                <i className="fas fa-search" />
+                Cari Lowongan
+              </a>
+              <a href="/editprofile">
+                <i className="fas fa-user" />
+                Edit Profile
+              </a>
+              <a href="/lamaranterkirim">
+                <i className="fas fa-paper-plane" />
+                Lamaran Terkirim
+              </a>
+              <a href="/undanganinterview">
+                <i className="fas fa-info-circle" />
+                Undangan Interview
+              </a>
+              <a href="/jadwalinterview">
+                <i className="fas fa-calendar-day" />
+                Jawdal Interview
+              </a>
+              <a href="/lamarantersimpan">
+                <i className="fas fa-archive" />
+                Lamaran Tersimpan
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
