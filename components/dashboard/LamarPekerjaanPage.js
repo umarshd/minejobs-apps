3e2d3e132dcd4bef6a9aff4a2eeda429261ee69e
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-24 19:07:55 +0700

    Update LamarPekerjaanPage.js

--- components/dashboard/LamarPekerjaanPage.js
+++ components/dashboard/LamarPekerjaanPage.js
@@ -208,39 +208,6 @@ export default function LamarPekerjaanPage() {
                 </div>
               </div>
             </div>
-            <div className="row justify-content-center mt-4">
-              <div className="col-4">
-                <nav aria-label="Page navigation example ">
-                  <ul className="pagination">
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        Previous
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        1
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        2
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        3
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        Next
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </div>
           </div>
         </div>
       </div>
