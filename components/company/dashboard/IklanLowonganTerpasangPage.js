0625c1ee47919ba3f48c6d4c970068c479d8edb2
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-28 01:46:45 +0700

    Create IklanLowonganTerpasangPage.js

---
+++ components/company/dashboard/IklanLowonganTerpasangPage.js
@@ -0,0 +1,138 @@
+import React from "react";
+
+export default function IklanLowonganTerpasangPage() {
+  return (
+    <>
+      <div className="col-md-12">
+        <div className="alert alert-custom-company" role="alert">
+          <div className="container-custom">
+            <span>Status Akun : Aktif</span>
+            <span className="pl-5">Sisa Credit : 5</span>
+            <span className="pl-5">Iklan Tayang : 3</span>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-12 ">
+        <div className="bg-custom-secondary pb-4 mb-3 pt-4">
+          <div className="container-custom">
+            <div className="row">
+              <div className="col-md-9">Iklan Dibuat</div>
+              <div className="col-md-3">
+                <a href="#" className="btn btn-secondary">
+                  Buat Iklan Baru
+                </a>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="container-custom">
+            <div className="card p-4">
+              <div className="row">
+                <div className="col-md-9">
+                  <div className="posisi font-weight-bold">
+                    Fullstack Web Developer
+                  </div>
+                  <div className="masaTayang">
+                    Masa Tayang Berakhir 2 April 2020
+                  </div>
+                  <div className="status">Belum Diproses : 10</div>
+                  <div className="jumlahTerpilih">Terpilih: 4</div>
+                  <div className="jumlahWawancara">Wawancara: 6 </div>
+                </div>
+                <div className="col-md-3">
+                  <a href="#" className="btn btn-primary btn-block">
+                    Lihat Iklan
+                  </a>
+                  <a href="#" className="btn btn-success btn-block">
+                    Lihat Kandidat
+                  </a>
+                  <a href="#" className="btn btn-warning btn-block">
+                    Tayangkan Kembali
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card p-4">
+              <div className="row">
+                <div className="col-md-9">
+                  <div className="posisi font-weight-bold">
+                    Fullstack Web Developer
+                  </div>
+                  <div className="masaTayang">
+                    Masa Tayang Berakhir 2 April 2020
+                  </div>
+                  <div className="status">Belum Diproses : 10</div>
+                  <div className="jumlahTerpilih">Terpilih: 4</div>
+                  <div className="jumlahWawancara">Wawancara: 6 </div>
+                </div>
+                <div className="col-md-3">
+                  <a href="#" className="btn btn-primary btn-block">
+                    Lihat Iklan
+                  </a>
+                  <a href="#" className="btn btn-success btn-block">
+                    Lihat Kandidat
+                  </a>
+                  <a href="#" className="btn btn-warning btn-block">
+                    Tayangkan Kembali
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card p-4">
+              <div className="row">
+                <div className="col-md-9">
+                  <div className="posisi font-weight-bold">
+                    Fullstack Web Developer
+                  </div>
+                  <div className="masaTayang">
+                    Masa Tayang Berakhir 2 April 2020
+                  </div>
+                  <div className="status">Belum Diproses : 10</div>
+                  <div className="jumlahTerpilih">Terpilih: 4</div>
+                  <div className="jumlahWawancara">Wawancara: 6 </div>
+                </div>
+                <div className="col-md-3">
+                  <a href="#" className="btn btn-primary btn-block">
+                    Lihat Iklan
+                  </a>
+                  <a href="#" className="btn btn-success btn-block">
+                    Lihat Kandidat
+                  </a>
+                  <a href="#" className="btn btn-warning btn-block">
+                    Tayangkan Kembali
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card p-4">
+              <div className="row">
+                <div className="col-md-9">
+                  <div className="posisi font-weight-bold">
+                    Fullstack Web Developer
+                  </div>
+                  <div className="masaTayang">
+                    Masa Tayang Berakhir 2 April 2020
+                  </div>
+                  <div className="status">Belum Diproses : 10</div>
+                  <div className="jumlahTerpilih">Terpilih: 4</div>
+                  <div className="jumlahWawancara">Wawancara: 6 </div>
+                </div>
+                <div className="col-md-3">
+                  <a href="#" className="btn btn-primary btn-block">
+                    Lihat Iklan
+                  </a>
+                  <a href="#" className="btn btn-success btn-block">
+                    Lihat Kandidat
+                  </a>
+                  <a href="#" className="btn btn-warning btn-block">
+                    Tayangkan Kembali
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
