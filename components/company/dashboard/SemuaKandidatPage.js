a3fe8bf7ee346bdb5f6582ca7a36950039f5a90b
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-28 01:46:40 +0700

    Create SemuaKandidatPage.js

---
+++ components/company/dashboard/SemuaKandidatPage.js
@@ -0,0 +1,224 @@
+import React from "react";
+
+export default function SemuaKandidatPage() {
+  return (
+    <>
+      <div className="col-md-12">
+        <div className="alert alert-custom-company" role="alert">
+          <div className="container-custom">
+            <span>Status Akun : Aktif</span>
+            <span className="pl-5">Sisa Kredit : 5</span>
+            <span className="pl-5">Iklan Tayang : 3</span>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-12">
+        <div className="container-custom mb-3">
+          <h4>Iklan Lowongan : Fullstack Web Developer</h4>
+          <div className="row">
+            <div className="col-md-3">
+              <a href="#" className="btn btn-warning btn-block">
+                Edit Iklan
+              </a>
+            </div>
+            <div className="col-md-3">
+              <a href="#" className="btn btn-danger btn-block">
+                Hentikan Iklan
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-12">
+        <div className="bg-custom-secondary pt-3 pb-3 mb-3">
+          <div className="container-custom">
+            <p className="font-weight-bold">Filter :</p>
+            <div className="row">
+              <div className="col-md-3">Belum Diproses (10)</div>
+              <div className="col-md-3">Tidak Sesuai (3)</div>
+              <div className="col-md-3">Kandidat Terpilih (5)</div>
+              <div className="col-md-3">List Wawancara (4)</div>
+              <div className="col-md-3">Wawancara Terdekat (5)</div>
+              <div className="col-md-3">Permintaan Atur Ulang (10)</div>
+              <div className="col-md-3">Menunggu Konfirmasi(2)</div>
+              <div className="col-md-3">Lihat Semua Jadwal (10)</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-12">
+        <div className="bg-custom-secondary pb-3 pt-3 mb-3">
+          <div className="container-custom">
+            <div className="row">
+              <div className="col-md-9">Menampilkan : Semua Kandidatat</div>
+              <div className="col-md-3">
+                <a href="#" className="btn btn-secondary">
+                  Tampilkan Semua Kandidat
+                </a>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-md-12">
+                <div className="card pt-3 pb-3">
+                  <div className="container-custom">
+                    <div className="row">
+                      <div className="col-md-9">
+                        <div className="nama-kandidat font-weight-bold">
+                          Agus Hartanto
+                        </div>
+                        <div className="pendidikan-kandidat">
+                          Pendidikan Terakhir: Sarjana (S1), Teknik Informatika
+                        </div>
+                        <div className="gaji-kandidat">
+                          Gaji yang di inginkan : Rp.3.000.000
+                        </div>
+                      </div>
+                      <div className="col-md-3">
+                        <a href="#" className="btn btn-primary btn-block">
+                          Lihat Profile
+                        </a>
+                        <a href="#" className="btn btn-success btn-block">
+                          Atur Wawancara
+                        </a>
+                        <a href="#" className="btn btn-warning btn-block">
+                          Pilih Kandidat
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card pt-3 pb-3">
+                  <div className="container-custom">
+                    <div className="row">
+                      <div className="col-md-9">
+                        <div className="nama-kandidat font-weight-bold">
+                          Agus Hartanto
+                        </div>
+                        <div className="pendidikan-kandidat">
+                          Pendidikan Terakhir: Sarjana (S1), Teknik Informatika
+                        </div>
+                        <div className="gaji-kandidat">
+                          Gaji yang di inginkan : Rp.3.000.000
+                        </div>
+                      </div>
+                      <div className="col-md-3">
+                        <a href="#" className="btn btn-primary btn-block">
+                          Lihat Profile
+                        </a>
+                        <a href="#" className="btn btn-success btn-block">
+                          Atur Wawancara
+                        </a>
+                        <a href="#" className="btn btn-warning btn-block">
+                          Pilih Kandidat
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card pt-3 pb-3">
+                  <div className="container-custom">
+                    <div className="row">
+                      <div className="col-md-9">
+                        <div className="nama-kandidat font-weight-bold">
+                          Agus Hartanto
+                        </div>
+                        <div className="pendidikan-kandidat">
+                          Pendidikan Terakhir: Sarjana (S1), Teknik Informatika
+                        </div>
+                        <div className="gaji-kandidat">
+                          Gaji yang di inginkan : Rp.3.000.000
+                        </div>
+                      </div>
+                      <div className="col-md-3">
+                        <a href="#" className="btn btn-primary btn-block">
+                          Lihat Profile
+                        </a>
+                        <a href="#" className="btn btn-success btn-block">
+                          Atur Wawancara
+                        </a>
+                        <a href="#" className="btn btn-warning btn-block">
+                          Pilih Kandidat
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="card pt-3 pb-3">
+                  <div className="container-custom">
+                    <div className="row">
+                      <div className="col-md-9">
+                        <div className="nama-kandidat font-weight-bold">
+                          Agus Hartanto
+                        </div>
+                        <div className="pendidikan-kandidat">
+                          Pendidikan Terakhir: Sarjana (S1), Teknik Informatika
+                        </div>
+                        <div className="gaji-kandidat">
+                          Gaji yang di inginkan : Rp.3.000.000
+                        </div>
+                      </div>
+                      <div className="col-md-3">
+                        <a href="#" className="btn btn-primary btn-block">
+                          Lihat Profile
+                        </a>
+                        <a href="#" className="btn btn-success btn-block">
+                          Atur Wawancara
+                        </a>
+                        <a href="#" className="btn btn-warning btn-block">
+                          Pilih Kandidat
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="container-custom">
+                  <div className="row justify-content-center mt-4">
+                    <div className="col-4">
+                      <nav aria-label="Page navigation example ">
+                        <ul className="pagination">
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              &lt;
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              1
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              2
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              3
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              &gt;
+                            </a>
+                          </li>
+                        </ul>
+                      </nav>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
