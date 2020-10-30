05930a2eeb50c6a71457da65c2a7bb46ad89fc76
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-10-30 16:59:39 +0700

    Update PerusahaanPage.js

--- components/company/admin/PerusahaanPage.js
+++ components/company/admin/PerusahaanPage.js
@@ -4,41 +4,87 @@ export default function PerusahaanPage() {
   return (
     <>
       <div className="col-md-7 col-lg-9">
-        <div className="card-right-job-detail p-3">
+        <div className="alert alert-custom-company" role="alert">
+          <div className="container mt-0">
+            <span>List Perusahaan</span>
+          </div>
+        </div>
+        <div className="card-right-job-detail p-5">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control bg-custom-3"
+              placeholder="Cari Perusahaan"
+            />
+            <div className="input-group-append">
+              <button className="btn bg-custom-3-btn" type="button">
+                <i className="fa fa-search" />
+              </button>
+            </div>
+          </div>
+
           <table className="table table-hover">
             <thead>
               <tr>
-                <th scope="col">Perusahaan</th>
-                <th scope="col">Status</th>
-                <th scope="col">Aksi</th>
+                <th scope="col" className="text-secondary">
+                  Perusahaan
+                </th>
+                <th scope="col" className="text-secondary">
+                  Status
+                </th>
+                <th scope="col" className="text-center text-secondary">
+                  Aksi
+                </th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>PT Panpanoe</td>
                 <td className="text-success">Aktif</td>
-                <a href="#" className="btn btn-primary">
-                  Detail
-                </a>
+                <td>
+                  <a
+                    href="/company/admin/detailperusahaan"
+                    className="btn bg-custom-2 btn-block"
+                  >
+                    detail
+                  </a>
+                </td>
               </tr>
               <tr>
                 <td>PT Karya Anak Bangsa</td>
                 <td className="text-warning">Menunggu Verifikasi</td>
-                <a href="#" className="btn btn-primary">
-                  Detail
-                </a>
+                <td>
+                  <a
+                    href="/company/admin/detailperusahaan"
+                    className="btn bg-custom-2 btn-block"
+                  >
+                    detail
+                  </a>
+                </td>
               </tr>
               <tr>
                 <td>NET Mediatama</td>
                 <td className="text-success">Aktif</td>
-                <a href="#" className="btn btn-primary">
-                  Detail
-                </a>
+                <td>
+                  <a
+                    href="/company/admin/detailperusahaan"
+                    className="btn bg-custom-2 btn-block"
+                  >
+                    detail
+                  </a>
+                </td>
               </tr>
               <tr className="">
                 <td>Google Inc</td>
                 <td className="text-danger">Dibatasi</td>
-                <td>detail</td>
+                <td>
+                  <a
+                    href="/company/admin/detailperusahaan"
+                    className="btn bg-custom-2 btn-block"
+                  >
+                    detail
+                  </a>
+                </td>
               </tr>
             </tbody>
           </table>
