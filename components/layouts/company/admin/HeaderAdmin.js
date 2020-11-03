98c1174bdd9d294c003d144997e37c2b9051cd75
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2020-11-03 22:14:09 +0700

    Update HeaderAdmin.js

--- components/layouts/company/admin/HeaderAdmin.js
+++ components/layouts/company/admin/HeaderAdmin.js
@@ -49,17 +49,17 @@ export default function HeaderAdmin() {
                   aria-haspopup="true"
                   aria-expanded="false"
                 >
-                  Keluar
+                  Budi
                 </button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a class="dropdown-item" href="#">
-                    Action
+                  <a class="dropdown-item" href="/company/dashboard/company">
+                    Company
+                  </a>
+                  <a class="dropdown-item" href="/company/admin/perusahaan">
+                    Admin
                   </a>
                   <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    Something else here
+                    Logout
                   </a>
                 </div>
               </div>
